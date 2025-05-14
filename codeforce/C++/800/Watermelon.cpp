d8ebafc1f8d54dfa74dd88cc40f928be315e9716
QuangPhung15 <phunghuyquang150904@gmail.com>
2025-05-14 01:03:49 -0400

    Test creating a CPP program

---
+++ codeforce/C++/800/Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int w;
+    cin >> w;
+
+    if (w % 2 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    solve();
+}
