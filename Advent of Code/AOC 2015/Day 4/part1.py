5939a1c500e43037d6f9fcb3599305230c893a9c
QuangPhung15 <phunghuyquang150904@gmail.com>
2025-12-25 22:56:32 -0500

    AOC 2015 Day 4

---
+++ Advent of Code/AOC 2015/Day 4/part1.py
@@ -0,0 +1,18 @@
+import hashlib
+
+with open("input.txt") as file:
+    data = file.read()
+    secret_key = data
+
+res = 1
+
+while (True):
+    input_str = secret_key + str(res)
+    output_str = hashlib.md5(input_str.encode("utf-8")).hexdigest()
+
+    if (output_str[:5] == "00000"):
+        break
+
+    res += 1
+
+print(res)
