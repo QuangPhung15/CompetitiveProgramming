94af6f61abbdd2ea391c3002b803f3af9865c74a
QuangPhung15 <phunghuyquang150904@gmail.com>
2026-01-12 12:20:25 -0800

    AOC 2016 Day 21

---
+++ Advent of Code/AOC 2016/Day 21/part1.py
@@ -0,0 +1,58 @@
+with open("input.txt") as file:
+    data = file.read().splitlines()
+    operations = []
+
+    for d in data:
+        line = d.split()
+
+        if (line[0] == "swap"):
+            if (line[1] == "position"):
+                operations.append(("sp", int(line[2]), int(line[5])))
+            else:
+                operations.append(("sl", line[2], line[5]))
+        elif (line[0] == "rotate"):
+            if (line[1] == "left"):
+                operations.append(("rl", int(line[2])))
+            elif (line[1] == "right"):
+                operations.append(("rr", int(line[2])))
+            else:
+                operations.append(("rb", line[6]))
+        elif (line[0] == "reverse"):
+            operations.append(("rev", int(line[2]), int(line[4])))
+        else:
+            operations.append(("mov", int(line[2]), int(line[5])))
+
+s = list("abcdefgh")
+n = len(s)
+
+for o in operations:
+    if (o[0] == "sp"):
+        i1, i2 = o[1], o[2]
+        s[i1], s[i2] = s[i2], s[i1]
+    elif (o[0] == "sl"):
+        i1, i2 = s.index(o[1]), s.index(o[2])
+        s[i1], s[i2] = s[i2], s[i1]
+    elif (o[0] == "rl"):
+        i = o[1] % n
+        s = s[i:] + s[:i]
+    elif (o[0] == "rr"):
+        i = o[1] % n
+        s = s[-i:] + s[:n - i] if (i > 0) else s
+    elif (o[0] == "rb"):
+        i = s.index(o[1])
+        k = (i + 2) % n if (i >= 4) else (i + 1) % n
+        s = s[-k:] + s[:n - k] if (k > 0) else s
+    elif (o[0] == "rev"):
+        l, r = o[1], o[2]
+
+        while (l < r):
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+    else:
+        i1, i2 = o[1], o[2]
+        c = s.pop(i1)
+        s.insert(i2, c)
+
+res = "".join(s)
+print(res)
