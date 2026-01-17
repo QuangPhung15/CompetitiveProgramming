f96e9c9f9771ec2bc4d91b9bebe8f0617a5080f3
QuangPhung15 <phunghuyquang150904@gmail.com>
2026-01-17 12:37:26 -0800

    AOC 2017 Day 9

---
+++ Advent of Code/AOC 2017/Day 9/part2.py
@@ -0,0 +1,38 @@
+with open("input.txt") as file:
+    data = file.read()
+    stream = data
+
+res = 0
+stack = []
+n = len(stream)
+i = 0
+
+while (i < n):
+    if (stream[i] == "!"):
+        i += 2
+    elif (stack and stack[-1][1] == "<"):
+        if (stream[i] == ">"):
+            stack.pop()
+        else:
+            res += 1
+        
+        i += 1
+    elif (stream[i] == "<"):
+        stack.append((-1, stream[i]))
+        i += 1
+    elif (stream[i] == ">"):
+        i += 1
+    elif (stream[i] == "{"):
+        if (stack):
+            stack.append((stack[-1][0] + 1, stream[i]))
+        else:
+            stack.append((1, stream[i]))
+        i += 1
+    elif (stream[i] == "}"):
+        if (stack and stack[-1][1] == "{"):
+            stack.pop()
+            i += 1
+    else:
+        i += 1
+
+print(res)
